5e102a1f2a997842e1a6361499991160cb2c3606
Koalhack <33071384+Koalhack@users.noreply.github.com>
2025-08-15 23:34:55 +0200

    feat(extras): add `fzf` theme

---
+++ extras/fzf/darcubox.ps1
@@ -0,0 +1,21 @@
+$env:FZF_DEFAULT_OPTS= @"
+  $env:FZF_DEFAULT_OPTS
+  --ansi
+  --color=fg:#d0c6a5
+  --color=fg+:#efead9
+  --color=bg:-1
+  --color=bg+:#25262C
+  --color=hl:#fb982e
+  --color=hl+:#dd4e21
+  --color=info:#d0c6a5
+  --color=marker:#ffd072
+  --color=prompt:#fb982e
+  --color=spinner:#52a260
+  --color=pointer:#cd80b9
+  --color=header:#9cd750
+  --color=gutter:#25262c
+  --color=border:#878080
+  --color=label:#d0c6a5
+  --color=query:#efead9:regular
+  --color=disabled:#404146
+"@
